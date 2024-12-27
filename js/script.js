0917dd29bb7cae2d9d59feece59b0e7600d8b28c
Miguel García Melgar <mikongame@gmail.com>
2024-12-27 18:44:04 +0100

    new site

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function loadComponent(containerId, filePath) {
+  fetch(filePath)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(containerId).innerHTML = data;
+      })
+      .catch(error => console.error("Error cargando el componente:", error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header-container", "components/header.html");
+  loadComponent("hero-container", "components/hero.html");
+  loadComponent("benefits-container", "components/benefits.html");
+  loadComponent("services-container", "components/services.html");
+  loadComponent("projects-container", "components/projects.html");
+  loadComponent("contact-container", "components/contact.html");
+  loadComponent("footer-container", "components/footer.html");
+});
+
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.querySelector("input[type='text']").value;
+  const email = document.querySelector("input[type='email']").value;
+  const message = document.querySelector("textarea").value;
+
+  if (name && email && message) {
+      alert("¡Gracias por contactarnos! Nos pondremos en contacto pronto.");
+  } else {
+      alert("Por favor, completa todos los campos.");
+  }
+});
